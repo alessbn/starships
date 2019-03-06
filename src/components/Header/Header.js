1526a4a1fb71533fe9a9eb4f8c68936643c4b884
Alejandra Bustos <alejandrabustosn@gmail.com>
2019-03-05 22:30:59 -0600

    Agregue componentes base

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return(
+    <header>
+      <img src="" alt=""/>
+      <h1>Starships</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Nav() {
+  return(
+    <aside>
+      <ul>
+        <li>Home</li>
+      </ul>
+    </aside>
+  );
+}
+
+export default Nav;
